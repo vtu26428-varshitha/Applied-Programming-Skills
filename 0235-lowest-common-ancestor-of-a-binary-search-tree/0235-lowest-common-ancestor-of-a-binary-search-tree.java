15fa72bafade2510f275b5b685be5bc3be05857d
vtu26428-varshitha <vtu26428@veltech.edu.in>
2026-04-14 18:10:38 +0530

    Time: 6 ms (97.15%), Space: 47.9 MB (22.9%) - LeetHub

---
+++ 0235-lowest-common-ancestor-of-a-binary-search-tree/0235-lowest-common-ancestor-of-a-binary-search-tree.java
@@ -0,0 +1,16 @@
+class Solution {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while (root != null) {
+            if (p.val < root.val && q.val < root.val) {
+                root = root.left;
+            } 
+            else if (p.val > root.val && q.val > root.val) {
+                root = root.right;
+            } 
+            else {
+                return root; // This is the LCA
+            }
+        }
+        return null;
+    }
+}
