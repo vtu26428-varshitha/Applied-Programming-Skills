e097c73142cf6073a923888797835941320cf6ae
vtu26428-varshitha <vtu26428@veltech.edu.in>
2026-05-08 00:10:12 +0530

    Time: 2 ms (99.56%), Space: 53.5 MB (83.02%) - LeetHub

---
+++ 0997-find-the-town-judge/0997-find-the-town-judge.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] indegree = new int[n + 1];
+        int[] outdegree = new int[n + 1];
+
+        for (int[] t : trust) {
+            outdegree[t[0]]++;
+            indegree[t[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (indegree[i] == n - 1 && outdegree[i] == 0) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
